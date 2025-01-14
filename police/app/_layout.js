44e1cd8fbfb29f404aa4e30c2f94cc551631369e
Prajwal B M <bmprajwal43@gmail.com>
2025-01-14 19:00:37 +0530

    Initial police UI

---
+++ police/app/_layout.js
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="(tab)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
